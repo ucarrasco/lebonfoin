693a9e5b98013e477507e69b192ef0556941d6a0
Ugo Carrasco <carrasco.ugo@gmail.com>
2017-11-26 21:29:36 +0100

    fixed wrong parsing of images

--- crawler/parser/itemDetailPage.js
+++ crawler/parser/itemDetailPage.js
@@ -5,14 +5,29 @@ export default itemHtml => {
   const $ = cheerio.load(itemHtml)
 
   let detailPageData = {
-    description: $('<p>' + $('[itemprop=description]').html().replace(/<br>/g, "\n") + '</p>').text().trim()
-  }
+    description: $('<p>' + $('[itemprop=description]').html().replace(/<br>/g, "\n") + '</p>').text().trim(),
+    images: (_ => {
+      // no image
+      if (!$('.item_image > .lazyload').length) return []
 
-  if ($('.thumbnails').length)
-    Object.assign(detailPageData, {
-      // numberOfImages: $('.thumbnails > ul > li').toArray().length,
-      images: itemHtml.match(/images\[\d\] = "[^"]+";/g).map(matchLine => matchLine.match(/images\[\d\] = "([^"]+)";/)[1]).map(imgUrl => ({ url: imgUrl, thumbUrl: imgUrl.replace('/ad-large/', '/ad-thumb/')}))
-    })
+      let multipleImagesMatch = itemHtml.match(/images\[\d\] = "[^"]+";/g)
+      
+      // multiple images
+      if (multipleImagesMatch)
+        return multipleImagesMatch
+            .map(matchLine => matchLine.match(/images\[\d\] = "([^"]+)";/)[1])
+            .map(imgUrl => ({
+              url: imgUrl, 
+              thumbUrl: imgUrl.replace('/ad-large/', '/ad-thumb/')
+            }))
+
+      // one image
+      return [{
+        url: $('.item_image > .lazyload').data('imgsrc').replace('/ad-image/', '/ad-large/'),
+        thumbUrl: $('.item_image > .lazyload').data('imgsrc').replace('/ad-image/', '/ad-thumb/')
+      }]
+    })()
+  }
 
   return detailPageData
 }
