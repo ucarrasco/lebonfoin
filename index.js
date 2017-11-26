9bb04520d94146facd7ac6af37d94d9e1e4b672f
Ugo Carrasco <carrasco.ugo@gmail.com>
2017-11-26 23:57:33 +0100

    reorganized persisting code to a module

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 import crawl from './crawler'
-
 import dbConnectThen from './helpers/dbHelper'
 import mongoose from 'mongoose'
-import Item from './models/Item'
+import persistCrawledItems from './crawlPersister'
 
 dbConnectThen(
   _ =>
     crawl('https://www.leboncoin.fr/ameublement/offres/languedoc_roussillon/herault/')
-      .then(
-        itemsList =>
-          Promise.all(itemsList.map(
-            crawledItem => 
-              Item.findOne({ remoteId: crawledItem.id })
-                .then(item => item ?
-                    item
-                    : new Item({
-                      remoteId: crawledItem.id,
-                      url: crawledItem.itemUrl,
-                      title: crawledItem.title,
-                      description: crawledItem.description,
-                      price: crawledItem.price,
-                      images: crawledItem.images
-                    })
-                      .save()
-                      .then(item => {
-                        console.log('save ok!', crawledItem)
-                        return item
-                      })
-                )
-              
-          ))
-      )
+      .then(persistCrawledItems)
       .then(items => {
-        // console.log(items.length)
+        console.log(items.length)
       })
 )
 
