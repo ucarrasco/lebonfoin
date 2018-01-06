7635bfdc0f415ee2c0b51fbebcae4292b0a0bc25
Ugo Carrasco <carrasco.ugo@gmail.com>
2018-01-06 01:14:55 +0100

    moved some express config to... express config module

--- config/express.js
+++ config/express.js
@@ -7,6 +7,12 @@ export default app => {
   app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
   }))
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  })
   
   app.use(function(err, req, res, next) {
     console.error(err.stack);
